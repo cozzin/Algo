74d2c5efb86d74626b19631df4bc2a2c18b37e86
seongho.hong <hsh3592@gmail.com>
2021-03-29 00:56:15 +0900

    LeetCode 1 풀이 추가

--- Algo/Logic/LeetCode/L1TowSum.swift
+++ Algo/Logic/LeetCode/L1TowSum.swift
@@ -18,7 +18,8 @@ extension LeetCode.L1 {
      주어진 배열의 길이가 최대 1000 이라서
      모든 경우의 수를 계산하는것도 가능하다고 판단함
      
-     시간복잡도는 O(N^2)
+     시간복잡도: O(N^2)
+     공간복잡도: O(1)
      */
     class Solution {
         func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
@@ -30,6 +31,33 @@ extension LeetCode.L1 {
                         return [i, j]
                     }
                 }
+            }
+            
+            return []
+        }
+    }
+    
+    /*
+     문제 풀이 보고 다시 코딩해봄
+     시간복잡도를 더 제한적으로 사용하는 경우 시도할 수 있는 풀이법
+     
+     시간복잡도: O(N)
+     공간복잡도: O(N)
+     */
+    class Solution2 {
+        func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+            var dict: [Int: Int] = [:]
+            
+            for (i, num) in nums.enumerated() {
+                // target - num = 해당값 찾기
+                // 문제가 요구하는 num + 해당값 = target이 됨
+                if let index = dict[target - num] {
+                    return [index, i]
+                }
+                
+                // 배열을 전진하면서 탐색하기 때문에
+                // 나중에 검색될 수 있게 딕셔너리에 넣어줘야 함
+                dict[num] = i
             }
             
             return []
